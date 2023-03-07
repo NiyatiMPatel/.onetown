9545c1be670679def6f63d8704417889eb8a6889
Niyati M Patel <patelnitie@gmail.com>
2023-03-07 10:22:54 +0530

    updated font-awesome link

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,10 @@
 // HEADER //
 var header = document.getElementById("myHeader");
 
- // STICKY-HEADER // 
-window.onscroll = function() {myFunction()};
+// STICKY-HEADER //
+window.onscroll = function () {
+  myFunction();
+};
 
 var sticky = header.offsetTop;
 
@@ -17,62 +19,64 @@ function myFunction() {
 //STARTING SECTION ONE BELOW STICKY HEADER //
 var belowHeader = header.nextElementSibling;
 
-belowHeader.style.marginTop = header.clientHeight +"px";
-
+belowHeader.style.marginTop = header.clientHeight + "px";
 
 // HOMEPAGE-SECTION-ONE-BACKGROUND CAROUSEL //
 var slider_content = document.getElementById("homepage-background");
-var image=['banner-background' , 'sample-1' , 'sample-2' , 'sample-3' , 'sample-4'];
+var image = [
+  "banner-background",
+  "sample-1",
+  "sample-2",
+  "sample-3",
+  "sample-4",
+];
 var i = 0;
 function carousel(direction) {
-  if(direction === 'next') {
+  if (direction === "next") {
     /* 
       1- i less than image length => increament the i
       2- i equal to image length => set i to 0
     */
-   if(i === image.length - 1) {
-    i = 0;
-   } else {
-     i++;
-   }
+    if (i === image.length - 1) {
+      i = 0;
+    } else {
+      i++;
+    }
   } else {
     /*
       if i ==0 then got last length index
       if i < 0 then decremenet i
     */
-      if(i === 0) {
-        i = image.length - 1;
-      } else {
-        i--;
-      }
+    if (i === 0) {
+      i = image.length - 1;
+    } else {
+      i--;
+    }
   }
   slider_content.src = `./images/${image[i]}.png`;
 }
 
-
 // SLICK-SLIDER //
-$('.slick-carousel').slick({
+$(".slick-carousel").slick({
   infinite: true,
   slidesToShow: 1, // Shows a three slides at a time
   slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
   variableWidth: true,
   // centerMode: true,
   arrows: false,
-  dots: true // Adds the dots on the bottom
+  dots: true, // Adds the dots on the bottom
 });
 
 // CONTACT-US-PAGE-RANGE-SELECTOR //
-var sliderOne= document.getElementById("sliderOne");
-var sliderTwo= document.getElementById("sliderTwo");
+var sliderOne = document.getElementById("sliderOne");
+var sliderTwo = document.getElementById("sliderTwo");
 
 var progressBarOne = document.getElementById("progressBarOne");
 var progressBarTwo = document.getElementById("progressBarTwo");
 
-
-sliderOne.oninput = function(){
-  progressBarOne.style.width =this.value + "%";
-}
-sliderTwo.oninput = function(){
-  progressBarTwo.style.width =this.value + "%";
-}
-
+sliderOne.oninput = function () {
+  progressBarOne.style.width = this.value + "%";
+};
+sliderTwo.oninput = function () {
+  progressBarTwo.style.width = this.value + "%";
+};
